1c65c38201876338c9ce82a142fa3d12754a539a
mcshayla <mcshayla@byu.edu>
2026-01-31 13:11:13 -0700

    workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+//   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+//   { languageOptions: { globals: globals.jest } },
+
+// ]);
+
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.jest } },
+  pluginJs.configs.recommended,
+];
